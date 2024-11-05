8ac6dfc5f7d2f53a176aaafaa2e4296db676b83a
dinhlan17122011 <dinhlan17122011@gmail.com>
2024-11-05 11:28:05 +0700

    ok

--- servers/src/config/connectDatabase.js
+++ servers/src/config/connectDatabase.js
@@ -2,17 +2,19 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+// Tải biến môi trường từ file .env
 dotenv.config();
 
-
+// Lấy đường dẫn của tệp hiện tại
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
+// Định nghĩa cấu hình kết nối
 const connectDB = async () => {
     const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
-        dialect: 'mysql', 
+        dialect: 'mysql', // hoặc 'postgres', 'sqlite', v.v.
     });
 
     try {
